301745a7d03bfe075ce17da1e795285b41d843bf
sdesrocher <sdesrocher@LAB24HR14-2TWY>
2017-11-09 16:10:58 -0600

    Request 405- Append log warnings

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -6,9 +6,13 @@ namespace SingleResponsibilityPrinciple
 {
     public class ConsoleLogger : ILogger
     {
-        public void LogWarning(string message, params object[] args)
+        public void LogWarning(string type, string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><type>" + type + "</type><message>" + message + "</message></log> ", args);
+            }
         }
 
         public void LogInfo(string message, params object[] args)
